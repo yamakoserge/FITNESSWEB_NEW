09d65d30b09580c7d93ef66724d23b7875154246
Serges YAMAKO <yamakoserges@gmail.com>
2024-08-23 13:48:09 +0100

    Calling get all Activities API in angular

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -2,42 +2,54 @@ import { Component } from '@angular/core';
 import { ShareModule } from '../../share/share.module';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-activity',
   standalone: true,
   imports: [ShareModule],
   templateUrl: './activity.component.html',
-  styleUrl: './activity.component.scss'
+  styleUrl: './activity.component.scss',
 })
 export class ActivityComponent {
-
   gridStyle = {
     width: '100%',
-    textAlign: 'center'
+    textAlign: 'center',
   };
   activityForm!: FormGroup;
+  activites:any;
 
+  constructor(
+    private fb: FormBuilder,
+    private message: NzMessageService,
+    private userService: UserService
+  ) {}
 
-  constructor( private fb: FormBuilder,
-  private message: NzMessageService,
-  private userService: UserService
-  ){}
-
-  ngOnInit(){
+  ngOnInit() {
     this.activityForm = this.fb.group({
       caloriesBurned: [null, [Validators.required]],
       steps: [null, [Validators.required]],
-      distance: [null,[Validators.required]],
+      distance: [null, [Validators.required]],
       date: [null, [Validators.required]],
-    })
+    });
+
+    this.getAllActivities();
   }
-  submitForm(){
-    this.userService.postActivity(this.activityForm.value).subscribe(res=>{
-      this.message.success("Activity posted successfully", {nzDuration:3000});
-      this.activityForm.reset();
-       } , error=>{
-        this.message.error("ErrorWhile posting Activity", {nzDuration:3000});
-       })
+  submitForm() {
+    this.userService.postActivity(this.activityForm.value).subscribe(
+      res => {
+        this.message.success('Activity posted successfully', {
+          nzDuration: 3000});
+        this.activityForm.reset();
+        this.getAllActivities();
+      }, error => {
+        this.message.error('ErrorWhile posting Activity', { nzDuration: 3000 });
+      })
+  }
+  getAllActivities(){
+    this.userService.getActivities().subscribe(res=> {
+      this.activites = res;
+      console.log(this.activites);
+    })
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URl =" http://localhost:8090/";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+ 
+  constructor(private http: HttpClient) {}
+
+  postActivity(activityDto: any): Observable<any> {
+    return this.http.post(BASIC_URl + 'api/activity', activityDto);
+  }
+
+  getActivities(): Observable<any>{
+    return this.http.get(BASIC_URl + 'api/activities');
+  }
+}
